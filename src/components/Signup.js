954ac97fbdbe8a06d8c25f0062f4d97b971d9c87
amolsasane <amolsasane001@gmail.com>
2024-05-31 18:01:57 +0530

    feat: form validaion

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,20 @@
+import { checkValidData } from "../utils/validate";
+import { useRef, useState } from "react";
+
 const Signup = ({ toggle }) => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const clickHandler = () => {
+    // validate the data entered
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
+
   return (
-    <div className="absolute bg-black bg-opacity-70 w-[28%] h-[70%] mx-auto my-[4rem] left-0 right-0 rounded-[1rem] p-14">
+    <div className="absolute bg-black bg-opacity-70 w-[28%] h-[75%] mx-auto my-[4rem] left-0 right-0 rounded-[1rem] p-14">
       <h1 className="text-white text-[2rem] font-bold mb-8">Sign Up</h1>
       <input
         className="p-4 my-2 w-full text-white bg-white bg-opacity-20 rounded-md"
@@ -8,19 +22,25 @@ const Signup = ({ toggle }) => {
         placeholder="Full Name"
       ></input>
       <input
+        ref={email}
         className="p-4 my-2 w-full text-white bg-white bg-opacity-20 rounded-md"
         type="text"
         placeholder="Email"
       ></input>
       <input
+        ref={password}
         className="p-4 my-2 w-full text-white bg-white bg-opacity-20 rounded-md"
         type="password"
         placeholder="Password"
       ></input>
-      <button className="bg-red-700 hover:contrast-100 text-white w-full p-4 my-2 rounded-md contrast-150 font-bold">
+      <p className="text-red-400 text-xs my-2">{errorMessage}</p>
+      <button
+        onClick={clickHandler}
+        className="bg-red-700 hover:contrast-100 text-white w-full p-4 my-2 rounded-md contrast-150 font-bold"
+      >
         Sign Up
       </button>
-      <p className="text-gray-400 mt-10">
+      <p className="text-gray-400 mt-4">
         Already a member?{" "}
         <span
           className="text-white hover:underline cursor-pointer"
---
+++ src/utils/validate.js
@@ -0,0 +1,13 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password
+    );
+
+  if (!isEmailValid) return "Enter valid Email address";
+  if (!isPasswordValid)
+    return "Password should be a combination of uppercase and lowercase letters, numbers, and symbols and have at last 8 charachters";
+
+  return null;
+};

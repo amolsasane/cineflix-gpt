f5d6f1db8e3b42f96e60b4136b41d6c020045af5
amolsasane <amolsasane001@gmail.com>
2024-06-03 17:30:15 +0530

    standard regex for email

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,7 @@
 export const checkValidData = (email, password) => {
-  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email
+  );
   const isPasswordValid =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
       password

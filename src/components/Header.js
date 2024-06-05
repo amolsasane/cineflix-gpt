e4988855fb2a241f96429e4d557edebafda67b5d
amolsasane <amolsasane001@gmail.com>
2024-06-05 17:39:52 +0530

    style: gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import logo from "../utils/images/logo.png";
 
 const Header = ({ toggle, isSignedUp }) => {
   return (
-    <div className="flex flex-between justify-between items-center">
+    <div className="flex flex-between justify-between items-center bg-gradient-to-b from-black relative w-full">
       <img
         className="w-[15rem] brightness-100 contrast-150 ml-[10rem]"
         src={logo}
         alt="logo"
       />
       <button
-        className="bg-red-700 hover:contrast-100 contrast-150 px-4 py-2 rounded-md m-4 text-white font-bold h-full content-center mr-[20rem]"
+        className="bg-red-700 hover:contrast-100 contrast-150 px-4 py-2 rounded-md mt-10 text-white font-bold h-full content-center mr-[20rem]"
         onClick={toggle}
       >
         {isSignedUp ? "Sign Up" : "Login"}

93a66eb855f5b84535a5af8351b9fca76ff5177e
amolsasane <amolsasane001@gmail.com>
2024-06-06 15:56:20 +0530

    user login logout state updation and feeding user credentials to redux store

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Form from "./Form";
 import Browse from "./Browse";
+import Error from "./Error";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -13,7 +21,20 @@ const Body = () => {
       path: "/browse",
       element: <Browse />,
     },
+    {
+      path: "/error",
+      element: <Error />,
+    },
   ]);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const { uid, email, displayName } = user;
+      dispatch(addUser({ uid: uid, email: email, name: displayName }));
+    } else {
+      dispatch(removeUser());
+    }
+  });
 
   return (
     <div>

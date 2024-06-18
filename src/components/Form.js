618a4aa08da1d3b144332fc70949a488508aecb9
amolsasane <amolsasane001@gmail.com>
2024-06-18 12:36:10 +0530

    created constants

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Login from "./Login";
 import Signup from "./Signup";
+import { bgImage } from "../utils/constants";
 
 const Form = () => {
   const [isSignedUp, setIsSignedUp] = useState(true);
@@ -14,7 +15,7 @@ const Form = () => {
     <div className="relative min-h-screen">
       <img
         className="h-full w-full absolute brightness-50"
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/a99688ca-33c3-4099-9baa-07a2e2acb398/ca15fd28-b624-4852-8bfe-9cdd5c88475d/IN-en-20240520-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        src={bgImage}
         alt="background"
       />
       <div className="relative z-10">

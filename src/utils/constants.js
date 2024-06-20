0c7039a64f910facd836e280be370c0726b774f7
amolsasane <amolsasane001@gmail.com>
2024-06-20 11:35:49 +0530

    TMDB data fetch & movie slice & custom hook

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
 export const bgImage =
   "https://assets.nflxext.com/ffe/siteui/vlv3/a99688ca-33c3-4099-9baa-07a2e2acb398/ca15fd28-b624-4852-8bfe-9cdd5c88475d/IN-en-20240520-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjEwM2Q5MGI5OTIyNjlhNjUxMjMxYzg4ZWQyZWIyNCIsInN1YiI6IjY2NzJkMjhmMTY5MDkxZmI5MmEzYTAwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HObYqYwN2GvKKPbxPCwoHfapliSnRnZxPsm-QkCLdOA",
+  },
+};

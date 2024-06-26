3575de869881fdb6f9576bbd35b21ebfdf306970
amolsasane <amolsasane001@gmail.com>
2024-06-26 22:12:18 +0530

    styles changed

--- src/components/MovieTrailer.js
+++ src/components/MovieTrailer.js
@@ -8,11 +8,11 @@ const MovieTrailer = ({ id }) => {
   return (
     <div>
       <iframe
-        className="w-screen aspect-video"
+        className="w-full aspect-video"
         src={
           "https://www.youtube.com/embed/" +
           movieTrailer?.key +
-          "?&autoplay=1&mute=1"
+          "?rel=0&autoplay=1&mute=1"
         }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"

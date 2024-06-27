ad1b4dddcf60d09e9ef6a75d6453ce50950dec6f
amolsasane <amolsasane001@gmail.com>
2024-06-27 11:37:29 +0530

    styles: paddig given

--- src/components/MovieCarausel.js
+++ src/components/MovieCarausel.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 const MovieCarousel = ({ title, movies }) => {
   return (
     <div>
-      <h1 className="ml-3 text-white font-bold text-xl rounded">{title}</h1>
+      <h1 className="ml-12 text-white font-bold text-2xl rounded">{title}</h1>
       <div className="slider">
         <div className="slide flex mt-2 mb-14">
           {movies?.map((movie) => (

6ac15a6fbc10cdd6b72cde52d00c2ee134db4ba6
amolsasane <amolsasane001@gmail.com>
2024-07-01 15:02:27 +0530

    lang constants

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,49 @@
+const langConstants = {
+  en: {
+    search: "Search",
+    gptSearchPlaceholder: "What would you like to watch today ?",
+    gptHeading: "Unlimited movies, find your favorite!",
+  },
+  hindi: {
+    search: "खोजें",
+    gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे ?",
+    gptHeading: "असीमित फिल्में, अपनी पसंदीदा ढूंढें!",
+  },
+  marathi: {
+    search: "शोधा",
+    gptSearchPlaceholder: "तुम्हाला आज काय बघायला आवडेल?",
+    gptHeading: "अमर्यादित चित्रपट, तुमचे आवडते शोधा!",
+  },
+  sanskrit: {
+    search: "समागम्",
+    gptSearchPlaceholder: "अद्य भवन्तः किं द्रष्टुम् इच्छन्ति ?",
+    gptHeading: "असीमित चलचित्रम्, स्वस्य प्रियं ज्ञातव्यम्!",
+  },
+  spanish: {
+    search: "Buscar",
+    gptSearchPlaceholder: "¿Qué te gustaría ver hoy?",
+    gptHeading: "Películas ilimitadas, encuentra tu favorita!",
+  },
+  germen: {
+    search: "Suchen",
+    gptSearchPlaceholder: "Was möchtest du heute sehen?",
+    gptHeading: "PUnbegrenzte Anzahl an Filmen, finden Sie Ihren Favoriten!",
+  },
+  french: {
+    search: "Chercher",
+    gptSearchPlaceholder: "Qu’aimeriez-vous voir aujourd’hui ?",
+    gptHeading: "Films illimités, trouvez votre préféré !",
+  },
+  chinese: {
+    search: "寻找",
+    gptSearchPlaceholder: "今天你想看什么？",
+    gptHeading: "无限电影，找到你最喜欢的！",
+  },
+  japanese: {
+    search: "探す",
+    gptSearchPlaceholder: "今日は何を見たいですか？",
+    gptHeading: "見放題の動画でお気に入りを見つけてください！",
+  },
+};
+
+export default langConstants;

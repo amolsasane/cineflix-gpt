91ceaf446afdb96b1fa47eb6365ae7cd478b396c
amolsasane <amolsasane001@gmail.com>
2024-07-01 15:04:57 +0530

    change lang feature and gpt search page

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import langConstants from "../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 const GptSearch = () => {
-  return <div>GptSearch</div>;
+  const lang = useSelector((store) => store.lang.selectedLang);
+
+  return (
+    <div className="text-white">
+      <h1 className="text-5xl font-bold p-2 m-2">
+        {langConstants[lang].gptHeading}
+      </h1>
+      <form className="m-2 p-2 flex justify-center">
+        <input
+          className="p-4 text-white placeholder-white text-2xl m-1 w-[60%] bg-blue-300 bg-opacity-20 rounded-md border border-2"
+          type="text"
+          placeholder={langConstants[lang].gptSearchPlaceholder}
+        />
+        <button className="bg-red-700 hover:contrast-100 contrast-150 font-bold p-4 m-1 text-2xl rounded-md text-white content-center">
+          {langConstants[lang].search} <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default GptSearch;
--- src/components/HeaderBrowse.js
+++ src/components/HeaderBrowse.js
@@ -6,13 +6,16 @@ import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { toggleGptSearch } from "../utils/gptSlice";
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import { changeLang } from "../utils/langSlice";
 
 const HeaderBrowse = () => {
   const user = useSelector((store) => store.user);
+  const showGpt = useSelector((store) => store.gpt.showGptSearch);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleSignIn = () => {
+  const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
@@ -36,38 +39,72 @@ const HeaderBrowse = () => {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [dispatch, navigate]);
 
   const handleGptSearchBtn = () => {
     dispatch(toggleGptSearch());
   };
 
+  const langChangeHandler = (e) => {
+    dispatch(changeLang(e.target.value));
+  };
+
   return (
-    <div className="h-screen bg-transparent flex justify-between items-start bg-gradient-to-b from-black absolute w-full pt-4">
+    <div
+      className={`flex justify-between items-start absolute w-full pt-4 ${
+        showGpt ? "gpt-header" : "default-header"
+      }`}
+    >
       <img
         className="w-[10rem] brightness-100 contrast-150 ml-10"
         src={logo}
         alt="logo"
       />
 
-      <div className="flex mr-10 mt-4">
-        <h1 className="text-white text-10 content-center">
-          Hello, {user?.name}
-        </h1>
+      <div className="flex items-center mr-10 mt-4 space-x-3">
+        {!showGpt && (
+          <>
+            <h1 className="text-white text-10 content-center">
+              Hello, {user?.name}
+            </h1>
 
-        <button
-          onClick={handleGptSearchBtn}
-          className="bg-white hover:contrast-100 contrast-150 text-sm px-4 font-bold pt-1 pb-2 ml-3 mt-1 rounded-md content-center"
-        >
-          GPT Search
-        </button>
+            <button
+              onClick={handleGptSearchBtn}
+              className="bg-white contrast-150 text-sm px-4 font-bold pt-1 pb-2 rounded-md content-center"
+            >
+              GPT Search
+            </button>
 
-        <button
-          className="bg-red-700 hover:contrast-100 contrast-150 text-sm px-4 font-bold pt-1 pb-2 ml-3 mt-1 rounded-md text-white content-center"
-          onClick={handleSignIn}
-        >
-          Logout
-        </button>
+            <button
+              className="bg-red-700 hover:contrast-100 contrast-150 text-sm px-4 font-bold pt-1 pb-2 rounded-md text-white content-center"
+              onClick={handleSignOut}
+            >
+              Logout
+            </button>
+          </>
+        )}
+
+        {showGpt && (
+          <>
+            <select
+              className="bg-gray-900 rounded-md text-white p-2"
+              onChange={langChangeHandler}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+
+            <button
+              onClick={handleGptSearchBtn}
+              className="bg-white contrast-150 px-4 font-bold pt-1 pb-2 rounded-md content-center ml-auto"
+            >
+              Home
+            </button>
+          </>
+        )}
       </div>
     </div>
   );

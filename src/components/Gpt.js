5d12d2aba5722137879cfa51114b676dda076ccb
amolsasane <amolsasane001@gmail.com>
2024-07-01 15:03:48 +0530

    bg image for gpt

--- src/components/Gpt.js
+++ src/components/Gpt.js
@@ -1,9 +1,22 @@
 import React from "react";
 import GptSearch from "./GptSearch";
+import { bgImage } from "../utils/constants";
 
 const Gpt = () => {
+  const divStyle = {
+    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)), linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)), url(${bgImage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
   return (
-    <div>
+    <div style={divStyle}>
       <GptSearch />
     </div>
   );

032ea18d0dc5e851613c77e5644beb8689ac4574
amolsasane <amolsasane001@gmail.com>
2024-07-05 18:36:08 +0530

    env changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjEwM2Q5MGI5OTIyNjlhNjUxMjMxYzg4ZWQyZWIyNCIsInN1YiI6IjY2NzJkMjhmMTY5MDkxZmI5MmEzYTAwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HObYqYwN2GvKKPbxPCwoHfapliSnRnZxPsm-QkCLdOA",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 

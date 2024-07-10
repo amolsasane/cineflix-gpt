307ac8b99888d444a705b3ac9e6a6595bb9cd0f9
amolsasane <amolsasane001@gmail.com>
2024-07-10 13:47:28 +0530

    memoized CH to stop unnecesory api calls

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const useNowPlayingMovies = () => {
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,7 +19,7 @@ const useNowPlayingMovies = () => {
       dispatch(addNowPlayingMovies(json.results));
     };
 
-    getNowPlayingMovies();
+    !nowPlayingMovies && getNowPlayingMovies();
   }, [dispatch]);
 };
 

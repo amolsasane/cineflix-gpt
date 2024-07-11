809073c7310094ad3b8a16a4918a07e115eee93f
amolsasane <amolsasane001@gmail.com>
2024-07-12 00:19:55 +0530

    feat: loader, auto scroll , bg image

--- src/components/Gpt.js
+++ src/components/Gpt.js
@@ -1,16 +1,10 @@
 import React from "react";
 import GptSearch from "./GptSearch";
 import GptResults from "./GptResults";
-import { bgImage } from "../utils/constants";
 
 const Gpt = () => {
   return (
-    <div
-      className="min-h-screen bg-cover bg-fixed"
-      style={{
-        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)), linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)), url(${bgImage})`,
-      }}
-    >
+    <div>
       <GptSearch />
       <GptResults />
     </div>

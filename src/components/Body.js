43a8ac86df1e04a8e4fdbe8ec49711e8f49f1f04
amolsasane <amolsasane001@gmail.com>
2024-07-20 00:47:22 +0530

    feat: landing page

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,16 @@ import Form from "./Form";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import PlayMovie from "./PlayMovie";
+import LandingPage from "./LandingPage";
 
 const Body = () => {
   const appRouter = createBrowserRouter([
     {
       path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
       element: <Form />,
     },
     {

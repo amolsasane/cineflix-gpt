6f18bf9db72ff5c3db505e266bea2b0d99a25cb2
amolsasane <amolsasane001@gmail.com>
2024-07-22 23:41:11 +0530

    style: responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -85,7 +85,7 @@ const Footer = () => {
           </div>
 
           <div className="w-full lg:flex-1 lg:pt-5">
-            <div className="grid grid-cols-1 gap-6 text-center sm:text-left sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 lg:mt-0">
+            <div className="grid grid-cols-1 gap-6 text-center md:ml-[7rem] sm:ml-[10rem] lg:ml-0 sm:text-left sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 lg:mt-0">
               <div>
                 <h3 className="text-white font-bold uppercase">About</h3>
                 <a

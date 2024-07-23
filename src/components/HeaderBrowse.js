0a33015afedea093199fd45847361554534f29e9
amolsasane <amolsasane001@gmail.com>
2024-07-23 21:54:33 +0530

    style: responsive header-browse

--- src/components/HeaderBrowse.js
+++ src/components/HeaderBrowse.js
@@ -48,33 +48,33 @@ const HeaderBrowse = () => {
 
   return (
     <div
-      className={`flex justify-between items-start absolute z-10 w-full pt-4 ${
+      className={`flex justify-between items-start absolute z-10 w-full bg-black lg:bg-transparent pb-2 lg:pb-0 lg:pt-4 ${
         showGpt ? "gpt-header" : "default-header"
       }`}
     >
       <img
-        className="w-[10rem] brightness-100 contrast-150 ml-10"
+        className="w-[5rem] lg:w-[10rem] brightness-100 contrast-150 ml-3 mt-1 lg:mt-0 lg:ml-10"
         src={logo}
         alt="logo"
       />
 
-      <div className="flex items-center mr-10 mt-4 space-x-3 pt-2 pr-4">
+      <div className="flex items-center mr-2 lg:mr-10 mt-0 lg:mt-4 space-x-0 lg:space-x-3 pt-2 lg:pr-4">
         {!showGpt && (
           <>
-            <h1 className="text-white text-sm px-4">
+            <h1 className="hidden lg:inline-block text-white text-sm px-4">
               Hello, {user?.name?.split(" ")?.filter((word) => word)[0]}
             </h1>
 
             <button
               onClick={handleGptSearchBtn}
-              className="text-sm mx-6 p-2 text-white hover:border hover:rounded-lg"
+              className="text-xs lg:text-sm mx-6 p-1 lg:p-2 text-white hover:border hover:rounded-lg"
             >
-              <img className="w-6 inline-flex" src={aiLogo} alt="ai" /> AI
-              Search
+              <img className="w-4 lg:w-6 inline-flex" src={aiLogo} alt="ai" />{" "}
+              AI Search
             </button>
 
             <button
-              className="text-sm  hover:border hover:rounded-lg py-2 px-4  text-white"
+              className="text-xs lg:text-sm  hover:border hover:rounded-lg py-1 lg:py-2 px-2 lg:px-4  text-white"
               onClick={handleSignOut}
             >
               Logout
@@ -85,14 +85,14 @@ const HeaderBrowse = () => {
         {showGpt && (
           <>
             <select
-              className="text-white p-2 bg-transparent hover:border hover:rounded-lg mr-10"
+              className="text-white p-1 lg:p-2 bg-transparent hover:border hover:rounded-lg mr-5 lg:mr-10 text-xs lg:text-[1rem]"
               onChange={langChangeHandler}
             >
               {SUPPORTED_LANGUAGES.map((lang) => (
                 <option
                   key={lang.identifier}
                   value={lang.identifier}
-                  className="text-black"
+                  className="text-black text-xs lg:text-md"
                 >
                   {lang.name}
                 </option>
@@ -101,7 +101,7 @@ const HeaderBrowse = () => {
 
             <button
               onClick={handleGptSearchBtn}
-              className="text-white text-md hover:border hover:rounded-lg pt-1 pb-1 px-3"
+              className="text-white text-xs lg:text-[1rem] hover:border hover:rounded-lg py-1 lg:py-2 px-3"
             >
               Home
             </button>
